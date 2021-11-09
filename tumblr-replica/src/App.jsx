aa1706760b75f85c44284c4a4a599b62610dc2aa
Essam <essamwisam@outlook.com>
2021-11-09 14:29:04 +0200

    Initialized the Project.
    To install all the required dependencies use "npm install"

---
+++ tumblr-replica/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <h1>Tumblr</h1>
+    </Router>
+  );
+}
+
+export default App;
