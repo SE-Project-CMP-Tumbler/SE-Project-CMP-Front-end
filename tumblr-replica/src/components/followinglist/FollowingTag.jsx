0d42c56642eb8b09006028f4520994049cabd34e
Raghad-Khaled <raghoda1234692000@gmail.com>
2021-11-16 23:21:50 +0200

    link following tag with the page of the tag

--- tumblr-replica/src/components/followinglist/FollowingTag.jsx
+++ tumblr-replica/src/components/followinglist/FollowingTag.jsx
@@ -5,6 +5,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 // import InboxIcon from '@mui/icons-material/Inbox';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function FollowingTag({ imagUrl, tag }) {
   const border = {
@@ -12,28 +13,31 @@ export default function FollowingTag({ imagUrl, tag }) {
     width: 80,
     height: 60,
   };
+  const linkst = { width: '100%', textDecoration: 'none' };
   return (
     <ListItem disablePadding>
-      <ListItemButton>
-        <ListItemIcon sx={{
-          width: 90, height: 60, borderRadius: '50%', mr: 0.5,
-        }}
-        >
-          <img
-            src={imagUrl}
-            alt="followingblog"
-            style={border}
-          />
-        </ListItemIcon>
-        <ListItemText
-          primary={`#${tag}`}
-          primaryTypographyProps={{
-            fontSize: 15,
-            letterSpacing: 0,
-            color: 'rgb(255,255,255)',
+      <Link to={`/tagged/${tag}`} style={linkst}>
+        <ListItemButton>
+          <ListItemIcon sx={{
+            width: 90, height: 60, borderRadius: '50%', mr: 0.5,
           }}
-        />
-      </ListItemButton>
+          >
+            <img
+              src={imagUrl}
+              alt="followingblog"
+              style={border}
+            />
+          </ListItemIcon>
+          <ListItemText
+            primary={`#${tag}`}
+            primaryTypographyProps={{
+              fontSize: 15,
+              letterSpacing: 0,
+              color: 'rgb(255,255,255)',
+            }}
+          />
+        </ListItemButton>
+      </Link>
     </ListItem>
   );
 }
