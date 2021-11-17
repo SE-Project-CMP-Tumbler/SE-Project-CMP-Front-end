0bef34d7309cd65304b9df7bfeead93756e6a945
Raghad-Khaled <raghoda1234692000@gmail.com>
2021-11-18 00:24:54 +0200

    tag card rander the data but json server not the best

--- tumblr-replica/src/components/tagcard/TagCard.jsx
+++ tumblr-replica/src/components/tagcard/TagCard.jsx
@@ -5,36 +5,50 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTaginfo, fetchAsynctag } from '../../states/features/tag/tagSlice';
 
 export default function TagCard() {
   const buttonst = {
     width: '50%',
-    color: '#122943',
+    // color: '#122943',
   };
   const cardColor = {
     backgroundColor: '#122943',
     color: 'white',
   };
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(fetchAsynctag());
+  }, []);
+  const taginfo = useSelector(getTaginfo);
+  // console.log(taginfo);
   return (
     <Card sx={{ maxWidth: 345 }} style={cardColor}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="https://www.ukrgate.com/eng/wp-content/uploads/2021/02/The-Ukrainian-Book-Institute-Purchases-380.9-Thousand-Books-for-Public-Libraries1.jpeg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          #funny memes
-        </Typography>
-        <Typography variant="body2" sx={{ fontWeight: 'bold' }} color="text.dark">
-          58k  followers
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" size="large" style={buttonst}>follow</Button>
-        <Button variant="contained" size="large" style={buttonst}>New post</Button>
-      </CardActions>
+      { taginfo.meta.status === '200'
+        ? (
+          <div>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image={taginfo.response.tag_image}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {`#${taginfo.response.tag_description}`}
+              </Typography>
+              <Typography variant="body2" sx={{ fontWeight: 'bold' }} color="text.dark">
+                { `${taginfo.response.followers_number}  followers` }
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant={taginfo.response.followed ? 'outlined' : 'contained'} size="large" style={buttonst}>{taginfo.response.followed ? 'unFollow' : 'Follow'}</Button>
+              <Button variant="contained" size="large" style={buttonst}>New post</Button>
+            </CardActions>
+          </div>
+        )
+        : (<h3>{ taginfo.meta.msg }</h3>)}
     </Card>
   );
 }
--- tumblr-replica/src/states/store.js
+++ tumblr-replica/src/states/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import followtagsReducer from './features/followtags/followtagsSlice';
+import tagreducer from './features/tag/tagSlice';
 
 const store = configureStore({
   reducer: {
     followtags: followtagsReducer,
+    tag: tagreducer,
   },
 });
 
