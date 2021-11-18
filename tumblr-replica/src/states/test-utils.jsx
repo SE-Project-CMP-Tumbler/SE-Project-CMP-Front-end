b91c82faf599b4ce693fde4ec3af08bb8085c424
Raghad-Khaled <raghoda1234692000@gmail.com>
2021-11-19 00:24:12 +0200

    unit test for tagCard is done

--- tumblr-replica/src/states/test-utils.jsx
+++ tumblr-replica/src/states/test-utils.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react'
+import { render as rtlRender } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 // Import your own reducer
-import tagSlice from './features/tag/tagSlice';
+// import PropTypes from 'prop-types';
+import tagReducer from './features/tag/tagSlice';
 
 function render(
   ui,
   {
     preloadedState,
-    store = configureStore({ reducer: { user: userReducer }, preloadedState }),
+    store = configureStore({ reducer: { tag: tagReducer }, preloadedState }),
     ...renderOptions
-  } = {}
+  } = {},
 ) {
+  // eslint-disable-next-line react/prop-types
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}>{children}</Provider>;
   }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
+  /* Wrapper.propTypes = {
+    children: PropTypes.objectOf(PropTypes.symbol).isRequired,
+  }; */
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
 // re-export everything
