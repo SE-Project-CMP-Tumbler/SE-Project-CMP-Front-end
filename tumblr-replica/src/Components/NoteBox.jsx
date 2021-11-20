ee3085f7167524c164dde72eb4863ddd6c0dc00a
maryemsalah22 <56718680+maryemsalah22@users.noreply.github.com>
2021-11-20 14:39:22 +0200

    Update NoteBox.jsx

--- tumblr-replica/src/Components/NoteBox.jsx
+++ tumblr-replica/src/Components/NoteBox.jsx
@@ -3,7 +3,6 @@ import Typography from '@mui/material/Typography';
 import '../Styles/notes.css';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
@@ -14,6 +13,10 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import '../Styles/post.css';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
 import LoggedIn from './Login';
 
 const NoteBox = function NoteBox() {
@@ -59,79 +62,98 @@ const NoteBox = function NoteBox() {
   };
   return (
     <>
-      <Grid container direction="row" maxWidth="100%">
-        <Grid item>
+      <Grid container direction="row" maxWidth="100%" xs={12} spacing={2} sx={{ mt: 0.5 }}>
+        <Grid item sx={{ ml: 0.9 }}>
           <Badge
             overlap="circular"
             anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-            badgeContent={
-              (
-                <RepeatIcon sx={{
+            badgeContent={(
+              <RepeatIcon
+                sx={{
                   width: 16,
                   height: 16,
                   bgcolor: '#00cc33',
                   fill: 'white',
                   borderRadius: '50%',
                 }}
-                />
-              )
-            }
+              />
+            )}
           >
-            <Avatar src="../img/heart.png" sx={{ width: 30, height: 30 }} alt="Travis Howard" />
+            <Avatar
+              src="../img/heart.png"
+              sx={{ width: 25, height: 25 }}
+              alt="Travis Howard"
+            />
           </Badge>
         </Grid>
         <Grid item style={{ height: 'inherit' }} xs={8}>
-          <Box bgcolor="white" style={{ display: 'inline-block', borderRadius: '5px', height: 'auto' }} sx={{ mr: 3, ml: 3 }} maxWidth="100%">
-            <Grid container direction="row">
-              <Grid item sx={{ pl: 2 }}>
-                <a href="https://www.google.com/?hl=en" cursor="pointer" textDecoration="none">blog</a>
-              </Grid>
-              <Grid item xs={10}>
-                <RepeatIcon fontSize="12px" sx={{ pt: 0.5 }} />
-              </Grid>
-              <Grid item xs={12}>
-                <div sx={{ width: 320, maxWidth: '100%' }}>
-                  <Button
-                    id="basic-button"
-                    aria-controls="basic-menu"
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={(e) => handleClick(e)}
-                  >
-                    <IconButton aria-label="More">
-                      <MoreHorizIcon />
-                    </IconButton>
-                  </Button>
-                  <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={() => handleClose()}
-                    MenuListProps={{
-                      'aria-labelledby': 'basic-button',
-                    }}
-                  >
-                    <MenuItem>props.post_date</MenuItem>
-                    <Divider />
-                    {(LoggedIn.blog_id === 2) && <MenuItem onClick={() => handleDelete()} style={{ color: 'red' }}>Delete reply</MenuItem>}
-                    {(LoggedIn.blog_id !== 2) && <MenuItem onClick={() => handleBlock()} style={{ color: 'red' }}>Block</MenuItem>}
-                    <MenuItem onClick={() => handleClose()}>Close</MenuItem>
-                  </Menu>
-                </div>
-              </Grid>
-            </Grid>
-            <Typography
+          <Card>
+            <CardHeader
               sx={{
-                pr: 2,
-                pl: 2,
-                pt: 2,
-                pb: 2,
+                pr: 0, pl: 1, pt: 0, pb: 0,
               }}
-              className="NoteText"
+              action={(
+                <Button
+                  id="basic-button"
+                  aria-controls="basic-menu"
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                  onClick={(e) => handleClick(e)}
+                >
+                  <IconButton aria-label="More">
+                    <MoreHorizIcon
+                      sx={{
+                        pr: 0, pl: 0, pt: 0, pb: 0,
+                      }}
+                    />
+                  </IconButton>
+                </Button>
+              )}
+              title={
+                (
+                  <Typography gutterBottom display="block" variant="caption"> Note owner name</Typography>
+                )
+               }
+            />
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={() => handleClose()}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
             >
-              maryem
-            </Typography>
-          </Box>
+              <MenuItem>props.post_date</MenuItem>
+              <Divider />
+              {LoggedIn.blog_id === 2 && (
+                <MenuItem
+                  onClick={() => handleDelete()}
+                  style={{ color: 'red' }}
+                >
+                  Delete reply
+                </MenuItem>
+              )}
+              {LoggedIn.blog_id !== 2 && (
+                <MenuItem
+                  onClick={() => handleBlock()}
+                  style={{ color: 'red' }}
+                >
+                  Block
+                </MenuItem>
+              )}
+              <MenuItem onClick={() => handleClose()}>Close</MenuItem>
+            </Menu>
+            <CardContent
+              sx={{
+                pr: 0, pl: 1, pt: 0, pb: 0,
+              }}
+            >
+              <Typography variant="body2" color="text.secondary">
+                <Box sx={{ fontWeight: 500 }}>Medium</Box>
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </>
