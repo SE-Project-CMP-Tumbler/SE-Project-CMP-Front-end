d1afef0f2d93742e1e53c61fa3311caed644dc17
Iten Elhak <56697800+Iten-No-404@users.noreply.github.com>
2021-12-30 16:42:59 +0200

    Blog Settings Half Finished
    + Delete User, Change Email & Change Password Tested

---
+++ tumblr-replica/src/states/blogsettingsslice/blogsettingsAPI.js
@@ -0,0 +1,123 @@
+/* eslint-disable max-len */
+import Axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { api, apiR } from '../../apis/globalAxpi';
+import { apiR as apiReal, SERVICETYPE, MOCK } from '../../apis/globalAPI';
+
+export const getBlogId = createAsyncThunk(
+  'blogSettings/info',
+  async (blogName, { getState }) => {
+    if (SERVICETYPE === MOCK) {
+      try {
+        const response = await api.get(`blog/info/${blogName}`);
+        return response.data.response;
+      } catch (e) {
+        throw Error(e);
+      }
+    } else {
+      try {
+        const state = getState();
+        const USERTOKEN = state.user.user.accessToken;
+        const AuthStr = `Bearer ${USERTOKEN}`;
+        const response = await apiR.get(`blog/info/${blogName}`, { headers: { Authorization: AuthStr } });
+        return response.data.response;
+      } catch (e) {
+        throw Error(e);
+      }
+    }
+  },
+);
+
+export const getBlogSettings = createAsyncThunk(
+  'blogSettings/getSettingas',
+  async (blogName, { getState }) => {
+    if (SERVICETYPE === MOCK) {
+      try {
+        const response = await api.get(`blog_settings/${blogName}`);
+        return response.data.response;
+      } catch (e) {
+        throw Error(e);
+      }
+    } else {
+      try {
+        const state = getState();
+        const USERTOKEN = state.user.user.accessToken;
+        const AuthStr = `Bearer ${USERTOKEN}`;
+        const response1 = await apiR.get(`blog/info/${blogName}`, { headers: { Authorization: AuthStr } });
+        const blogInfo = response1.data.response;
+        const response = await apiR.get(`blog_settings/${blogInfo.id}`, { headers: { Authorization: AuthStr } });
+        return { settings: response.data, blogInfo };
+      } catch (e) {
+        throw Error(e);
+      }
+    }
+  },
+);
+
+export const putBlogSettings = createAsyncThunk(
+  'blogSettings/info',
+  async (blogId, { getState }) => {
+    if (SERVICETYPE === MOCK) {
+      try {
+        const response = await api.put(`blog_settings/${blogId}`);
+        return response.data.response;
+      } catch (e) {
+        throw Error(e);
+      }
+    } else {
+      try {
+        const state = getState();
+        const USERTOKEN = state.user.user.accessToken;
+        const AuthStr = `Bearer ${USERTOKEN}`;
+        console.log(state.blogSettings.settings.submissions_settings.allow_submittions);
+        console.log(state.blogSettings.settings.submissions_settings.allow_submittions);
+
+        // const apt = state.blogSettings.settings.ask_settings.ask_page_title;
+        // const spt = state.blogSettings.settings.submissions_settings.submissions_page_title === undefined ? '' : state.blogSettings.settings.submissions_settings.submissions_page_title;
+        // const sg = state.blogSettings.settings.submissions_settings.submissions_guidelines === undefined ? '' : state.blogSettings.settings.submissions_settings.submissions_guidelines;
+        const response = await Axios({
+          method: 'PUT',
+          url: `${apiReal}/blog_settings/${blogId}`,
+          headers: {
+            Authorization: AuthStr,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          data: {
+            replies_settings: state.blogSettings.settings.replies_settings,
+            allow_ask: state.blogSettings.settings.ask_settings.allow_ask,
+            // ask_page_title: apt,
+
+            // allow_anonymous_questions: state.blogSettings.settings.ask_settings.allow_anonymous_questions,
+            // allow_submittions: state.blogSettings.settings.submissions_settings.allow_submittions,
+
+            // submissions_page_title: spt,
+            // submissions_guidelines: sg,
+            allow_messages: state.blogSettings.settings.allow_messages,
+            share_likes: state.blogSettings.settings.share_likes,
+            share_followings: state.blogSettings.settings.share_followings,
+          },
+        });
+        console.log('Share Likes Changed Successfully');
+        console.log(response.data);
+        // await apiR.put(`blog_settings/${blogId}`, {
+        //   headers: {
+        //     Authorization: AuthStr,
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        //   },
+        //   data: {
+        //     share_likes: state.blogSettings.settings.share_likes,
+        //   },
+        // });
+        return response.data;
+      } catch (e) {
+        console.log('Error!');
+        throw Error(e);
+      }
+    }
+  },
+);
+
+export const zero = 0;
+// nevermind this. otherwilse will have to use a default export.
